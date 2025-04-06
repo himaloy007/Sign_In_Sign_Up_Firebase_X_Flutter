fc9044c30dba91cd6bfa30817705d722d5a0424f
himaloy007 <ce22002@mbstu.ac.bd>
2025-04-06 17:07:00 +0600

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await PushNotificationService.init();
+  await PushNotificationService.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
